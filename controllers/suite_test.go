c758b665df3d3be7da1b3d74d30a2b9885bfc11e
Mohamed Mahmoud <mmahmoud@redhat.com>
2024-05-17 14:29:03 -0400

    add go mod vendor target and packages
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	bpfmaniov1alpha1 "github.com/bpfman/bpfman-operator/api/v1alpha1"
+	bpfmaniov1alpha1 "github.com/bpfman/bpfman-operator/apis/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
