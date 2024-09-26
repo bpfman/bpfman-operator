8c279d31af09cdd8f199b924e3a173cbeb1660ac
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-26 01:53:38 +0000

    chore(deps): update bpfman-operator-bundle to abb81f2
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- hack/update_catalog.sh
+++ hack/update_catalog.sh
@@ -3,7 +3,7 @@ set -eu
 
 #!/usr/bin/env bash
 
-export BPFMAN_OPERATOR_BUNDLE_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-operator-bundle@sha256:bbab5d2b708d856c14c4336730fd45ce51a61386dfbe118d65f4e9d5da484159"
+export BPFMAN_OPERATOR_BUNDLE_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-operator-bundle@sha256:abb81f28153a345ba4b6d9d79b1e0b60c076c97359a93773b2c63c0be4ac9fec"
 export BPFMAN_OPERATOR_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-rhel9-operator@sha256:41aaa6034949f2ed0f0946fa604246dc16f8e774a444784230a030ea4a3f7e6b"
 
 export INDEX_FILE=/configs/bpfman-operator/index.yaml
