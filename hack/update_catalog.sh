b6f49c616075e7774f79c39d9c7a83e4818e1faf
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-10-10 13:29:44 +0000

    chore(deps): update bpfman-operator-bundle to c7bb6f9
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- hack/update_catalog.sh
+++ hack/update_catalog.sh
@@ -3,7 +3,7 @@ set -eu
 
 #!/usr/bin/env bash
 
-export BPFMAN_OPERATOR_BUNDLE_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-operator-bundle@sha256:d75b89066a200c450da402445d8dbbf998e25a4e785507a67052baaeb3aa64cb"
+export BPFMAN_OPERATOR_BUNDLE_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-operator-bundle@sha256:c7bb6f9c6a21654a7579ec97813631214fee79651559c637c32f843231b61736"
 export BPFMAN_OPERATOR_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-rhel9-operator@sha256:81354d552775fd57404416a195bcb8ed2e9757e79d0b128fdd28a6b1ce4d502a"
 
 export INDEX_FILE=/configs/bpfman-operator/index.yaml
