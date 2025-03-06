6bd41ad25a55dd2ff8ab79a14f2a961ea7800208
Mohamed Mahmoud <mmahmoud@redhat.com>
2025-03-06 07:50:12 -0500

    WIP: Add interfaces discovery to bpf agent process
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- apis/v1alpha1/tc_program_types.go
+++ apis/v1alpha1/tc_program_types.go
@@ -32,25 +32,28 @@ type TcAttachInfo struct {
 
 	// containers identify the set of containers in which to attach the eBPF
 	// program.
-	Containers ContainerSelector `json:"containers"`
+	// +optional
+	Containers ContainerSelector `json:"containers,omitempty"`
 
 	// direction specifies the direction of traffic the tc program should
 	// attach to for a given network device.
 	// +kubebuilder:validation:Enum=Ingress;Egress
-	Direction TCDirectionType `json:"direction"`
+	// +optional
+	Direction TCDirectionType `json:"direction,omitempty"`
 
 	// priority specifies the priority of the tc program in relation to
 	// other programs of the same type with the same attach point. It is a value
 	// from 0 to 1000 where lower values have higher precedence.
 	// +kubebuilder:validation:Minimum=0
 	// +kubebuilder:validation:Maximum=1000
-	Priority int32 `json:"priority"`
+	// +optional
+	Priority int32 `json:"priority,omitempty"`
 
 	// proceedOn allows the user to call other tc programs in chain on this exit code.
 	// Multiple values are supported by repeating the parameter.
 	// +optional
 	// +kubebuilder:default:={Pipe,DispatcherReturn}
-	ProceedOn []TcProceedOnValue `json:"proceedOn"`
+	ProceedOn []TcProceedOnValue `json:"proceedOn,omitempty"`
 }
 
 type TcProgramInfoState struct {
