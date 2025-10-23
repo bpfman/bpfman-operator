bbd64488948cf61dbc36d3918d244731fd17ba17
Andreas Karis <ak.karis@gmail.com>
2025-10-23 14:43:09 +0200

    test-integration: Add priority ordering verification for BPF links
    Add integration tests to verify BPF program links are correctly ordered
    by priority across XDP, TC, and TCX program types.
    
    The new verification framework validates link ordering on each cluster
    node by comparing ClusterBpfApplicationState data against actual bpfman
    daemon state.
    
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- test/integration/tc_test.go
+++ test/integration/tc_test.go
@@ -5,7 +5,7 @@ package integration
 
 import (
 	"bytes"
-	"context"
+	"fmt"
 	"io"
 	"testing"
 	"time"
@@ -14,20 +14,23 @@ import (
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 )
 
 const (
 	tcGoCounterKustomize       = "https://github.com/bpfman/bpfman/examples/config/default/go-tc-counter/?timeout=120&ref=main"
 	tcGoCounterUserspaceNs     = "go-tc-counter"
 	tcGoCounterUserspaceDsName = "go-tc-counter-ds"
+	tcGoCounterBytecodeName    = "go-tc-counter-example"
+	tcByteCodeLabelSelector    = "app.kubernetes.io/name=tcprogram"
 )
 
 func TestTcGoCounter(t *testing.T) {
 	t.Log("deploying tc counter program")
 	require.NoError(t, clusters.KustomizeDeployForCluster(ctx, env.Cluster(), tcGoCounterKustomize))
-	addCleanup(func(context.Context) error {
+	t.Cleanup(func() {
 		cleanupLog("cleaning up tc counter program")
-		return clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), tcGoCounterKustomize)
+		clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), tcGoCounterKustomize)
 	})
 
 	t.Log("waiting for go tc counter userspace daemon to be available")
@@ -41,9 +44,80 @@ func TestTcGoCounter(t *testing.T) {
 
 	pods, err := env.Cluster().Client().CoreV1().Pods(tcGoCounterUserspaceNs).List(ctx, metav1.ListOptions{LabelSelector: "name=go-tc-counter"})
 	require.NoError(t, err)
+	require.Len(t, pods.Items, 1)
 	gotcCounterPod := pods.Items[0]
 
 	req := env.Cluster().Client().CoreV1().Pods(tcGoCounterUserspaceNs).GetLogs(gotcCounterPod.Name, &corev1.PodLogOptions{})
+
+	require.Eventually(t, func() bool {
+		logs, err := req.Stream(ctx)
+		require.NoError(t, err)
+		defer logs.Close()
+		output := new(bytes.Buffer)
+		_, err = io.Copy(output, logs)
+		require.NoError(t, err)
+		t.Logf("counter pod log %s", output.String())
+
+		return doTcCheck(t, output)
+	}, 30*time.Second, time.Second)
+}
+
+func TestTcGoCounterLinkPriority(t *testing.T) {
+	priorities := []*int32{
+		nil,
+		ptr.To(int32(0)),
+		ptr.To(int32(500)),
+		ptr.To(int32(1000)),
+	}
+
+	t.Log("deploying tc counter program")
+	require.NoError(t, clusters.KustomizeDeployForCluster(ctx, env.Cluster(), tcGoCounterKustomize))
+	t.Cleanup(func() {
+		cleanupLog("cleaning up tc counter program")
+		clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), tcGoCounterKustomize)
+
+		cleanupLog("cleaning up tc counter bytecode")
+		bpfmanClient.BpfmanV1alpha1().ClusterBpfApplications().DeleteCollection(ctx, metav1.DeleteOptions{},
+			metav1.ListOptions{
+				LabelSelector: tcByteCodeLabelSelector,
+			})
+	})
+
+	t.Log("creating copies of bytecode using the same link")
+	cba, err := bpfmanClient.BpfmanV1alpha1().ClusterBpfApplications().Get(ctx, tcGoCounterBytecodeName, metav1.GetOptions{})
+	require.NoError(t, err)
+	name := cba.Name
+	cba.ObjectMeta = metav1.ObjectMeta{
+		Labels: cba.Labels,
+	}
+	for i, priority := range priorities {
+		cba.Name = fmt.Sprintf("%s-%d", name, i)
+		cba.Spec.Programs[0].TC.Links[0].Priority = priority
+		_, err := bpfmanClient.BpfmanV1alpha1().ClusterBpfApplications().Create(ctx, cba, metav1.CreateOptions{})
+		require.NoError(t, err)
+	}
+	// Add priority 55 from the kustomize deployment as well.
+	priorities = append(priorities, ptr.To(int32(55)))
+
+	t.Log("waiting for bytecode to be attached successfully")
+	require.Eventually(t, clusterBpfApplicationStateSuccess(t, tcByteCodeLabelSelector, len(priorities)), 2*time.Minute, 10*time.Second)
+	require.Eventually(t, verifyClusterBpfApplicationPriority(t, tcByteCodeLabelSelector), 1*time.Minute, 10*time.Second)
+
+	t.Log("waiting for go tc counter userspace daemon to be available")
+	require.Eventually(t, func() bool {
+		daemon, err := env.Cluster().Client().AppsV1().DaemonSets(tcGoCounterUserspaceNs).Get(ctx, tcGoCounterUserspaceDsName, metav1.GetOptions{})
+		require.NoError(t, err)
+		return daemon.Status.DesiredNumberScheduled == daemon.Status.NumberAvailable
+	},
+		// Wait 5 minutes since cosign is slow, https://github.com/bpfman/bpfman/issues/1043
+		5*time.Minute, 10*time.Second)
+
+	pods, err := env.Cluster().Client().CoreV1().Pods(tcGoCounterUserspaceNs).List(ctx, metav1.ListOptions{LabelSelector: "name=go-tc-counter"})
+	require.NoError(t, err)
+	require.Len(t, pods.Items, 1)
+	goTcCounterPod := pods.Items[0]
+
+	req := env.Cluster().Client().CoreV1().Pods(tcGoCounterUserspaceNs).GetLogs(goTcCounterPod.Name, &corev1.PodLogOptions{})
 
 	require.Eventually(t, func() bool {
 		logs, err := req.Stream(ctx)
--- test/integration/tcx_test.go
+++ test/integration/tcx_test.go
@@ -5,7 +5,7 @@ package integration
 
 import (
 	"bytes"
-	"context"
+	"fmt"
 	"io"
 	"testing"
 	"time"
@@ -14,20 +14,23 @@ import (
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 )
 
 const (
 	tcxGoCounterKustomize       = "https://github.com/bpfman/bpfman/examples/config/default/go-tcx-counter/?timeout=120&ref=main"
 	tcxGoCounterUserspaceNs     = "go-tcx-counter"
 	tcxGoCounterUserspaceDsName = "go-tcx-counter-ds"
+	tcxGoCounterBytecodeName    = "go-tcx-counter-example"
+	tcxByteCodeLabelSelector    = "app.kubernetes.io/name=tcxprogram"
 )
 
 func TestTcxGoCounter(t *testing.T) {
 	t.Log("deploying tcx counter program")
 	require.NoError(t, clusters.KustomizeDeployForCluster(ctx, env.Cluster(), tcxGoCounterKustomize))
-	addCleanup(func(context.Context) error {
+	t.Cleanup(func() {
 		cleanupLog("cleaning up tcx counter program")
-		return clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), tcxGoCounterKustomize)
+		clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), tcxGoCounterKustomize)
 	})
 
 	t.Log("waiting for go tcx counter userspace daemon to be available")
@@ -41,9 +44,80 @@ func TestTcxGoCounter(t *testing.T) {
 
 	pods, err := env.Cluster().Client().CoreV1().Pods(tcxGoCounterUserspaceNs).List(ctx, metav1.ListOptions{LabelSelector: "name=go-tcx-counter"})
 	require.NoError(t, err)
+	require.Len(t, pods.Items, 1)
 	gotcxCounterPod := pods.Items[0]
 
 	req := env.Cluster().Client().CoreV1().Pods(tcxGoCounterUserspaceNs).GetLogs(gotcxCounterPod.Name, &corev1.PodLogOptions{})
+
+	require.Eventually(t, func() bool {
+		logs, err := req.Stream(ctx)
+		require.NoError(t, err)
+		defer logs.Close()
+		output := new(bytes.Buffer)
+		_, err = io.Copy(output, logs)
+		require.NoError(t, err)
+		t.Logf("counter pod log %s", output.String())
+
+		return doTcxCheck(t, output)
+	}, 30*time.Second, time.Second)
+}
+
+func TestTcxGoCounterLinkPriority(t *testing.T) {
+	priorities := []*int32{
+		nil,
+		ptr.To(int32(0)),
+		ptr.To(int32(500)),
+		ptr.To(int32(1000)),
+	}
+
+	t.Log("deploying tcx counter program")
+	require.NoError(t, clusters.KustomizeDeployForCluster(ctx, env.Cluster(), tcxGoCounterKustomize))
+	t.Cleanup(func() {
+		cleanupLog("cleaning up tcx counter program")
+		clusters.KustomizeDeleteForCluster(ctx, env.Cluster(), tcxGoCounterKustomize)
+
+		cleanupLog("cleaning up tcx counter bytecode")
+		bpfmanClient.BpfmanV1alpha1().ClusterBpfApplications().DeleteCollection(ctx, metav1.DeleteOptions{},
+			metav1.ListOptions{
+				LabelSelector: tcxByteCodeLabelSelector,
+			})
+	})
+
+	t.Log("creating copies of bytecode using the same link")
+	cba, err := bpfmanClient.BpfmanV1alpha1().ClusterBpfApplications().Get(ctx, tcxGoCounterBytecodeName, metav1.GetOptions{})
+	require.NoError(t, err)
+	name := cba.Name
+	cba.ObjectMeta = metav1.ObjectMeta{
+		Labels: cba.Labels,
+	}
+	for i, priority := range priorities {
+		cba.Name = fmt.Sprintf("%s-%d", name, i)
+		cba.Spec.Programs[0].TCX.Links[0].Priority = priority
+		_, err := bpfmanClient.BpfmanV1alpha1().ClusterBpfApplications().Create(ctx, cba, metav1.CreateOptions{})
+		require.NoError(t, err)
+	}
+	// Add priority 55 from the kustomize deployment as well.
+	priorities = append(priorities, ptr.To(int32(55)))
+
+	t.Log("waiting for bytecode to be attached successfully")
+	require.Eventually(t, clusterBpfApplicationStateSuccess(t, tcxByteCodeLabelSelector, len(priorities)), 2*time.Minute, 10*time.Second)
+	require.Eventually(t, verifyClusterBpfApplicationPriority(t, tcxByteCodeLabelSelector), 1*time.Minute, 10*time.Second)
+
+	t.Log("waiting for go tcx counter userspace daemon to be available")
+	require.Eventually(t, func() bool {
+		daemon, err := env.Cluster().Client().AppsV1().DaemonSets(tcxGoCounterUserspaceNs).Get(ctx, tcxGoCounterUserspaceDsName, metav1.GetOptions{})
+		require.NoError(t, err)
+		return daemon.Status.DesiredNumberScheduled == daemon.Status.NumberAvailable
+	},
+		// Wait 5 minutes since cosign is slow, https://github.com/bpfman/bpfman/issues/1043
+		5*time.Minute, 10*time.Second)
+
+	pods, err := env.Cluster().Client().CoreV1().Pods(tcxGoCounterUserspaceNs).List(ctx, metav1.ListOptions{LabelSelector: "name=go-tcx-counter"})
+	require.NoError(t, err)
+	require.Len(t, pods.Items, 1)
+	goTcxCounterPod := pods.Items[0]
+
+	req := env.Cluster().Client().CoreV1().Pods(tcxGoCounterUserspaceNs).GetLogs(goTcxCounterPod.Name, &corev1.PodLogOptions{})
 
 	require.Eventually(t, func() bool {
 		logs, err := req.Stream(ctx)
